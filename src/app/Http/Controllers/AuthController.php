b98fdaf6efb8284c4755371e56615b7fa2d36a5d
BOUCHAIB EL HAIDI <bouchaibelhaidi1@gmail.com>
2026-02-08 22:53:49 +0100

    feat: add logout method to AuthController

--- src/app/Http/Controllers/AuthController.php
+++ src/app/Http/Controllers/AuthController.php
@@ -48,5 +48,10 @@ return back()->withErrors([
 
 
 }
-
+    public function logout(Request $request) {
+        Auth::logout();
+        $request->session()->invalidate();
+        $request->session()->regenerateToken();
+        return redirect()->route('login');
+    }
 }
