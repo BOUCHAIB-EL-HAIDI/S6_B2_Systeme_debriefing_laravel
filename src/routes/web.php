b0822247e11c996dacfefd2f239a174481e76444
BOUCHAIB EL HAIDI <bouchaibelhaidi1@gmail.com>
2026-02-08 22:45:58 +0100

    feat: add Competence CRUD routes to web.php

--- src/routes/web.php
+++ src/routes/web.php
@@ -5,6 +5,9 @@ use App\Http\Controllers\AuthController;
 use App\Http\Controllers\AdminController;
 use App\Http\Controllers\ClasseController;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\SprintsController;
+use App\Http\Controllers\CompetenceController;
+
 Route::get('/', function () {
     return view('welcome');
 });
@@ -31,3 +34,18 @@ Route::post('/admin/users/create', [UserController::class, 'store'])->name('admi
 Route::get('/admin/users/{id}/edit', [UserController::class, 'edit'])->name('admin.users.edit');
 Route::put('/admin/users/{id}', [UserController::class, 'update'])->name('admin.users.update');
 Route::delete('/admin/users/{id}', [UserController::class, 'destroy'])->name('admin.users.destroy');
+
+
+//sprints routes
+
+Route::get('/admin/sprints', [SprintsController::class, 'showAndCreate'])->name('admin.sprints.showandcreate');
+Route::post('/admin/sprints', [SprintsController::class, 'store'])->name('admin.sprints.store');
+
+// Competence CRUD Routes
+Route::get('/admin/competences', [CompetenceController::class, 'index'])->name('admin.competences.index');
+Route::get('/admin/competences/create', [CompetenceController::class, 'create'])->name('admin.competences.create');
+Route::post('/admin/competences', [CompetenceController::class, 'store'])->name('admin.competences.store');
+Route::get('/admin/competences/{code}/edit', [CompetenceController::class, 'edit'])->name('admin.competences.edit');
+Route::put('/admin/competences/{code}', [CompetenceController::class, 'update'])->name('admin.competences.update');
+Route::delete('/admin/competences/{code}', [CompetenceController::class, 'destroy'])->name('admin.competences.destroy');
+
